382e116ef0c1b726e6d50ad25f2597971e3c5930
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-03-25 00:23:44 +1100

    add get all bill and get one bill by id for bill API endpoints

--- backend-smart-bills/src/auth/auth.service.ts
+++ backend-smart-bills/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+import { AuthDto } from './dto/auth.dto';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(User) private userRepository:Repository<User>){}
+    
+    async login(data:AuthDto, response:Response){
+        
+    }
+}
--- backend-smart-bills/src/bill/bill.service.ts
+++ backend-smart-bills/src/bill/bill.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Bill } from 'src/entities/bill.entity';
 @Injectable()
-export class BillService {}
+export class BillService {
+    constructor(@InjectRepository(Bill) private billRepository:Repository<Bill>){}
+
+    //get all bill
+    async getAllBill(){
+        try{
+            const data=await this.billRepository.find();
+            return {message: "All bill data are retrieved", data:data}
+        }
+        catch(error){
+            console.log(error);
+            throw new HttpException('Something wrong, please try again at later time', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //get one bill by id
+    async getOneBill(id:number){
+        if(!id){
+            throw new HttpException('Please provide id.', HttpStatus.OK);
+        }
+        try{
+            const bill=await this.billRepository.findOneBy({id});
+            return {message: "Bill data is retrieved", data:bill};
+        }
+        catch(error){
+            console.log(error);
+            throw new HttpException('Something wrong, please try again at later time', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //create new bill
+    
+}
