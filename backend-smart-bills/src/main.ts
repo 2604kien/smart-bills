4180341d355cf65670d951531c0cf12104379d21
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-03-28 17:57:09 +1100

    add session

--- backend-smart-bills/src/main.ts
+++ backend-smart-bills/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
+import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+      maxAge: 24*60*60*1000,
+      secure:true,
+      httpOnly: true,
+      sameSite:'none'
+    }
+  }))
   await app.listen(process.env.PORT||3500);
 }
 bootstrap();
